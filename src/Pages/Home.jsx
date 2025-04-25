eae9064d2546ec2d133c9fec90de4f009aaa8c01
Daniil Riazanov <ryazanow45@gmail.com>
2025-04-26 00:02:55 +0200

    Home Page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import FilmsCard from "../components/FilmsCard";
+
+export default function Home(){
+    return (
+        <main>
+      <div className="container">
+        <div className="main-text">
+          <h1 className="text-netflix">NETFLIX MOVIES</h1>
+          <h3 className="text-current">Current and New Releases</h3>
+        </div>
+        <div className="films">
+            {
+                [...Array(12)].map((_,index)=><FilmsCard key={index}/>)
+            }
+        </div>
+      </div>
+    </main>
+    )
+}
---
+++ src/components/FilmsCard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function FilmsCard() {
+  const [like, setLike] = useState(true);
+
+  function liked(e){
+    e.preventDefault()
+    setLike(!like)
+  }
+  return (
+    <a href="#" className="films-card">
+      <button
+        onClick={liked}
+        className={like ? "toggle-heart" : "toggle-heart liked"}
+        id="toggle-heart"
+        aria-label="like"
+      >
+        ❤
+      </button>
+      <img
+        src="https://new.kinogo.fm/uploads/posts/2022-03/251733_1647372642.webp"
+        alt=""
+      />
+      <div className="films-info-card">
+        <span className="film-name">Avatar</span>
+        <div className="movie-rating">
+          <span>&#9733;</span>
+          <span>7.5</span>
+        </div>
+        <div className="film-category">
+          <span className="name-category">Action</span>
+          <span className="name-category">Adventure</span>
+          <span className="name-category">Science Fiction</span>
+        </div>
+      </div>
+    </a>
+  );
+}
